7a3d4e0ae307d7395f3f4ac4e8695845e0527c4f
Rafael Audy Glanzner <rafael.audy@gmail.com>
2018-11-02 18:09:08 +0000

    Adds string reversal exercise

--- exercises/string-reversal.js
+++ exercises/string-reversal.js
@@ -1,7 +1,28 @@
+const reverse = string =>
+  string
+    .split("")
+    .reverse()
+    .join("");
+
+const reverseAlternative = string => {
+  let reverse = [];
+  let last = string.length - 1;
+
+  for (let initial = 0; initial < string.length; initial++) {
+    reverse[initial] = string[last];
+    reverse[last] = string[initial];
+    last--;
+  }
+
+  return reverse.join("");
+};
+
 const { assert } = require("chai");
 
 describe("String Reversal", () => {
   it("Should reverse string", () => {
     assert.equal(reverse("Hello World!"), "!dlroW olleH");
+    assert.equal(reverseAlternative("HelloWorld!"), "!dlroWolleH");
+    assert.equal(reverseAlternative("Hello World!"), "!dlroW olleH");
   });
 });

deb34ff950e3760ea3db4fca0fb6212b35b0d6ff
rafaelaudy <rafael.audy@gmail.com>
2018-11-03 12:48:51 +0000

    Reverse int cool one liner

--- exercises/int-reversal.js
+++ exercises/int-reversal.js
@@ -7,11 +7,26 @@ const reverse = int => {
   return parseInt(int > 0 ? inverted : `-${inverted}`);
 };
 
+const reverseAlternative = int => {
+  return (
+    parseInt(
+      int
+        .toString()
+        .split("")
+        .reverse()
+        .join("")
+    ) * Math.sign(int)
+  );
+};
+
 const { assert } = require("chai");
 
 describe("Integer Reversal", () => {
   it("Should reverse integer", () => {
     assert.equal(reverse(1234), 4321);
     assert.equal(reverse(-1200), -21);
+
+    assert.equal(reverseAlternative(1234), 4321);
+    assert.equal(reverseAlternative(-1200), -21);
   });
 });

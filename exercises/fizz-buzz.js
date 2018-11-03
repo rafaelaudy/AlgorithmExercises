5444f1a8fc0302b3710890935b45bb2430f6c583
rafaelaudy <rafael.audy@gmail.com>
2018-11-03 17:31:32 +0000

    Fizz buzz clever implementation

--- exercises/fizz-buzz.js
+++ exercises/fizz-buzz.js
@@ -14,20 +14,43 @@ const fizzBuzz = number => {
   return fizzBuzzArray;
 };
 
+const fizzBuzzAlternative = number => {
+  const fizzBuzzArray = [];
+  for (let counter = 1; counter <= number; counter++) {
+    if (counter % 6 === 0) {
+      fizzBuzzArray.push("Fizz Buzz");
+    } else if (counter % 2 === 0) {
+      fizzBuzzArray.push("Fizz");
+    } else if (counter % 3 === 0) {
+      fizzBuzzArray.push("Buzz");
+    } else {
+      fizzBuzzArray.push(counter);
+    }
+  }
+
+  return fizzBuzzArray;
+};
+
 const { assert } = require("chai");
 
 describe("Fizz Buzz", () => {
-  beforeEach(() => (output = fizzBuzz(30)));
+  let fizzBuzzOutput = fizzBuzz(30);
+  let fizzBuzzAlternativeOutput = fizzBuzzAlternative(30);
+
   it("Should output number", () => {
-    assert.equal(output[0], 1);
+    assert.equal(fizzBuzzOutput[0], 1);
+    assert.equal(fizzBuzzAlternativeOutput[0], 1);
   });
   it("Should output Fizz", () => {
-    assert.equal(output[1], "Fizz");
+    assert.equal(fizzBuzzOutput[1], "Fizz");
+    assert.equal(fizzBuzzAlternativeOutput[1], "Fizz");
   });
   it("Should output Buzz", () => {
-    assert.equal(output[2], "Buzz");
+    assert.equal(fizzBuzzOutput[2], "Buzz");
+    assert.equal(fizzBuzzAlternativeOutput[2], "Buzz");
   });
   it("Should output Fizz Buzz", () => {
-    assert.equal(output[5], "Fizz Buzz");
+    assert.equal(fizzBuzzOutput[5], "Fizz Buzz");
+    assert.equal(fizzBuzzAlternativeOutput[5], "Fizz Buzz");
   });
 });

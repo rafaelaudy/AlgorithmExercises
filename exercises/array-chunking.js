192177e9250b463fc653d6f893510121ce00ab61
rafaelaudy <rafael.audy@gmail.com>
2018-11-05 14:38:10 +0000

    Adds array chinking solution

---
+++ exercises/array-chunking.js
@@ -0,0 +1,25 @@
+const chunk = (array, chunkSize) =>
+  array.reduce(
+    (acc, item) => {
+      let currentChunkIndex = acc.length - 1;
+
+      if (acc[currentChunkIndex].length < chunkSize)
+        acc[currentChunkIndex].push(item);
+      else {
+        acc.push([item]);
+      }
+
+      return acc;
+    },
+    [[]]
+  );
+
+const { assert } = require("chai");
+
+describe("Array Chunking", () => {
+  it("Should implement array chunking", () => {
+    assert.deepEqual(chunk([1, 2, 3, 4], 2), [[1, 2], [3, 4]]);
+    assert.deepEqual(chunk([1, 2, 3, 4], 3), [[1, 2, 3], [4]]);
+    assert.deepEqual(chunk([1, 2, 3, 4], 5), [[1, 2, 3, 4]]);
+  });
+});

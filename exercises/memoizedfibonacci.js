2c6bbbfdbca15168cb7cb79083464a250c5dd58b
Rafael Audy Glanzner <rafael.audy@gmail.com>
2018-11-08 17:32:25 +0000

    Memoizes recursive fibonacci

---
+++ exercises/memoizedfibonacci.js
@@ -0,0 +1,27 @@
+const memoizedFibonacciMap = new Map();
+
+const fibonacci = number => {
+  if (number <= 2) return 1;
+
+  const memoizedFibonacci = memoizedFibonacciMap.get(number);
+  if (memoizedFibonacci) {
+    return memoizedFibonacci;
+  }
+
+  const fibonacciResult = fibonacci(number - 1) + fibonacci(number - 2);
+  memoizedFibonacciMap.set(number, fibonacciResult);
+
+  return fibonacciResult;
+};
+
+const { assert } = require("chai");
+
+describe("Fibonacci - recursive and memoized", () => {
+  it("Should implement fibonacci", () => {
+    assert.equal(fibonacci(1), 1);
+    assert.equal(fibonacci(2), 1);
+    assert.equal(fibonacci(3), 2);
+    assert.equal(fibonacci(6), 8);
+    assert.equal(fibonacci(10), 55);
+  });
+});

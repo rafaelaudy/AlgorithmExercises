bd7dd7bc44576eca44c3cf7d5b451dc54810cd9d
rafaelaudy <rafael.audy@gmail.com>
2018-11-13 19:21:49 +0000

    Adds more elegant solution for tree width

--- exercises/tree-width.js
+++ exercises/tree-width.js
@@ -24,6 +24,30 @@ const treeWidths = node => {
   return widtch;
 };
 
+const treeWidthMoreElegant = node => {
+  let nodes = [node, "nextLevel"];
+  let width = [];
+  let counter = 0;
+
+  while (nodes.length > 0) {
+    const nextNode = nodes.shift();
+
+    if (nextNode === "nextLevel") {
+      if (counter) {
+        nodes.push("nextLevel");
+        width.push(counter);
+      }
+
+      counter = 0;
+    } else {
+      counter++;
+      nodes.push(...nextNode.children);
+    }
+  }
+
+  return width;
+};
+
 const { assert } = require("chai");
 
 describe("Width of Tree Levels", () => {
@@ -33,5 +57,6 @@ describe("Width of Tree Levels", () => {
     root.add(3);
     root.children[1].add(4);
     assert.deepEqual(treeWidths(root), [1, 2, 1]);
+    assert.deepEqual(treeWidthMoreElegant(root), [1, 2, 1]);
   });
 });

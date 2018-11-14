f38f2d4af4199d41d112207c8d2cca4d93d9fd32
Rafael Audy Glanzner <rafael.audy@gmail.com>
2018-11-14 18:29:24 +0000

    Adds bubble sort sorting

---
+++ sorting/bubble-sort.js
@@ -0,0 +1,48 @@
+const bubbleSort = array => {
+  let shouldSort = true;
+
+  while (shouldSort) {
+    shouldSort = false;
+
+    for (let index = 0; index < array.length; index++) {
+      let current = array[index];
+      let next = array[index + 1] || current;
+
+      if (current > next) {
+        shouldSort = true;
+        array[index] = next;
+        array[index + 1] = current;
+      }
+    }
+  }
+
+  return array;
+};
+
+const _bubbleSort = array => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      let current = array[j];
+      let next = array[j + 1] || current;
+
+      if (current > next) {
+        shouldSort = true;
+        array[j] = next;
+        array[j + 1] = current;
+      }
+    }
+  }
+
+  return array;
+};
+
+const { assert } = require("chai");
+
+describe("Bubble Sort", () => {
+  it("Should implement bubble sort", () => {
+    assert.deepEqual(bubbleSort([5, 4, 3, 2, 1]), [1, 2, 3, 4, 5]);
+    assert.deepEqual(bubbleSort([5, 3, 1, 2, 4]), [1, 2, 3, 4, 5]);
+    assert.deepEqual(_bubbleSort([5, 4, 3, 2, 1]), [1, 2, 3, 4, 5]);
+    assert.deepEqual(_bubbleSort([5, 3, 1, 2, 4]), [1, 2, 3, 4, 5]);
+  });
+});

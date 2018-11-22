09d31df94e6af2b22cf5ff150eb4d4e8f7e73d40
Rafael Audy Glanzner <rafael.audy@gmail.com>
2018-11-22 18:08:45 +0000

    Adds route-between-nodes file - WIP

---
+++ search/route-between-nodes.js
@@ -0,0 +1,18 @@
+const Graph = require("../structures/graph");
+
+const { assert } = require("chai");
+
+describe("Route between nodes", () => {
+  it("Should find the quickest route between nodes", () => {
+    // Work in progress
+    // const b5 = new Node("b");
+    // const b4 = new Node("b");
+    // const b3 = new Node("b");
+    // const b2 = new Node("b");
+    // const b1 = new Node("b");
+    // const a = new Node("a");
+    // const b = new Node("b", [b1]);
+    // const graph = new Graph([a]);
+    // graph.addRoot(b);
+  });
+});

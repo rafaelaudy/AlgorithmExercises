b2a49f3edaa2e894ba7b73bd62d8ce7ae3819290
Rafael Audy Glanzner <rafael.audy@gmail.com>
2018-11-28 18:23:50 +0000

    Work in progress sorted array to bynary tree

---
+++ search/sorted-array-to-binary-tree.js
@@ -0,0 +1,9 @@
+const { Graph, Node } = require("../structures/graph-my-take");
+
+const sortedArrayToBinaryTree = sortedArray => {};
+
+const { assert } = require("chai");
+
+describe("Sorted array to binary tree", () => {
+  it("Should have the minimum height", () => {});
+});

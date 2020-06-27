7fed64c1a1cc6b708ea69f41da79c7c18520966a
Rafael Audy Glanzner <rafael.audy@gmail.com>
2020-06-27 12:12:25 +0100

    Moves files around

--- src/search/sorted-array-to-binary-tree.js
+++
@@ -1,9 +0,0 @@
-const { Graph, Node } = require("../structures/graph-my-take");
-
-const sortedArrayToBinaryTree = sortedArray => {};
-
-const { assert } = require("chai");
-
-describe("Sorted array to binary tree", () => {
-  it("Should have the minimum height", () => {});
-});

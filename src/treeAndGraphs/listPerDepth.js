45c749b84bd66be5756ce074f78173eb4a0150d0
Rafael Audy Glanzner <rafael.audy@gmail.com>
2020-06-29 17:53:34 +0100

    Adds list per depth

---
+++ src/treeAndGraphs/listPerDepth.js
@@ -0,0 +1,63 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+  }
+}
+
+//              5
+//     2                  8
+//  1     3          6         9
+//           4          7         10
+
+// [[5], [2, 8], [1, 3, 6, 9], [4, 7, 10]]
+
+const listPerDepth = (node) => {
+  const nodeQueue = [{ node: node, level: 0 }];
+  const result = [];
+
+  while (nodeQueue.length) {
+    const current = nodeQueue.pop();
+    if (result[current.level]) {
+      result[current.level].push(current.node.value);
+    } else {
+      result[current.level] = [current.node.value];
+    }
+
+    if (current.node.left)
+      nodeQueue.unshift({ node: current.node.left, level: current.level + 1 });
+    if (current.node.right)
+      nodeQueue.unshift({ node: current.node.right, level: current.level + 1 });
+  }
+
+  return result;
+};
+
+const prepareTree = () => {
+  const root = new Node(5);
+  root.left = new Node(2);
+  root.right = new Node(8);
+  root.left.left = new Node(1);
+  root.left.right = new Node(3);
+  root.left.right.right = new Node(4);
+  root.right.left = new Node(6);
+  root.right.right = new Node(9);
+  root.right.left.right = new Node(7);
+  root.right.right.right = new Node(10);
+  return root;
+};
+
+const { assert } = require("chai");
+
+describe("listPerDepth", () => {
+  it("Should get one array per level of the tree", () => {
+    const result = listPerDepth(prepareTree());
+    assert.deepEqual(result[0], [5]);
+    assert.deepEqual(result[1], [2, 8]);
+    assert.deepEqual(result[2], [1, 3, 6, 9]);
+    assert.deepEqual(result[3], [4, 7, 10]);
+  });
+});
+
+module.exports = {
+  listPerDepth,
+};

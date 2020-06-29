1b9bc0c39ca522b8e10f133f08e995ec1be56eda
Rafael Audy Glanzner <rafael.audy@gmail.com>
2020-06-29 18:00:26 +0100

    Adds list per depth (recursive version)

--- src/treeAndGraphs/listPerDepth.js
+++ src/treeAndGraphs/listPerDepth.js
@@ -32,6 +32,21 @@ const listPerDepth = (node) => {
   return result;
 };
 
+const listPerDepthRecursive = (node, level = 0, agg = []) => {
+  if (!node) return;
+
+  if (agg[level]) {
+    agg[level].push(node.value);
+  } else {
+    agg[level] = [node.value];
+  }
+
+  listPerDepthRecursive(node.left, level + 1, agg);
+  listPerDepthRecursive(node.right, level + 1, agg);
+
+  return agg;
+};
+
 const prepareTree = () => {
   const root = new Node(5);
   root.left = new Node(2);
@@ -51,6 +66,14 @@ const { assert } = require("chai");
 describe("listPerDepth", () => {
   it("Should get one array per level of the tree", () => {
     const result = listPerDepth(prepareTree());
+    assert.deepEqual(result[0], [5]);
+    assert.deepEqual(result[1], [2, 8]);
+    assert.deepEqual(result[2], [1, 3, 6, 9]);
+    assert.deepEqual(result[3], [4, 7, 10]);
+  });
+
+  it("Should get one array per level of the tree (recursive)", () => {
+    const result = listPerDepthRecursive(prepareTree());
     assert.deepEqual(result[0], [5]);
     assert.deepEqual(result[1], [2, 8]);
     assert.deepEqual(result[2], [1, 3, 6, 9]);

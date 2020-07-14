62cf03694f8222166f4b2afb12b175ada18a8b2e
Rafael Audy Glanzner <rafael.audy@gmail.com>
2020-07-14 17:39:14 +0100

    Finishes getBinarySearchTreeNextNode

--- src/treeAndGraphs/getBinarySearchTreeNextNode.js
+++ src/treeAndGraphs/getBinarySearchTreeNextNode.js
@@ -12,11 +12,31 @@ class Node {
   }
 }
 
+// is parent empty?
+//  return current next
 // is current on parent.next?
 //  return current next
 // else
 //  return parent
-const getBinarySearchTreeNextNode = (node) => {};
+
+const getBinarySearchTreeNextNode = (node) => {
+  if (!(node instanceof Node)) {
+    return undefined;
+  }
+
+  const parent = node.parent;
+
+  if (!parent || parent.right === node) {
+    let next = node.right;
+    while (next && next.left) {
+      next = next.left;
+    }
+
+    return next;
+  }
+
+  return parent;
+};
 
 const { assert } = require("chai");
 
@@ -33,12 +53,24 @@ const prepareGraph = () => {
 
   node5.left = node2;
   node5.right = node8;
+  node2.parent = node5;
+  node8.parent = node5;
+
   node2.left = node1;
   node2.right = node3;
+  node1.parent = node2;
+  node3.parent = node2;
+
   node8.left = node7;
   node8.right = node9;
+  node7.parent = node8;
+  node9.parent = node8;
+
   node7.left = node6;
+  node6.parent = node7;
+
   node9.right = node10;
+  node10.parent = node9;
 
   return node5;
 };
@@ -46,11 +78,24 @@ const prepareGraph = () => {
 describe("Tree", () => {
   beforeEach(prepareGraph);
 
-  it("Should get next item", () => {});
+  it("Should get next item", () => {
+    const root = prepareGraph();
+    assert.equal(getBinarySearchTreeNextNode(root).value, 6);
+    assert.equal(getBinarySearchTreeNextNode(root.right).value, 9);
+    assert.equal(getBinarySearchTreeNextNode(root.right.left).value, 8);
+  });
 
-  it("should return undefined when it is empty", () => {});
+  it("should return undefined when it is empty", () => {
+    assert.isUndefined(getBinarySearchTreeNextNode({}), undefined);
+  });
 
-  it("Should undefined when it is the last node", () => {});
+  it("Should undefined when it is the last node", () => {
+    const root = prepareGraph();
+    assert.equal(
+      getBinarySearchTreeNextNode(root.right.right.right),
+      undefined
+    );
+  });
 });
 
 module.exports = {
